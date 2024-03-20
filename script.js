42b2ed654e647e7f811c2443a271eef1847ba450
Internest Agency <133626843+internest-agency@users.noreply.github.com>
2024-03-20 09:42:42 +0530

    Get Final

---
+++ script.js
@@ -0,0 +1,11 @@
+const hero = $("#herobanner").offset().top;
+const journey = $("#journey").offset().top;
+const program = $("#program").offset().top;
+const sponsors = $("#sponsors").offset().top;
+const footer = $("#footer").offset().top;
+
+console.log(hero);
+console.log(journey);
+console.log(program);
+console.log(sponsors);
+console.log(footer);
